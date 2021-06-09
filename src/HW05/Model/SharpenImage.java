a4f996aa0cc339268996cf013ee1e4a1cf08ef66
bzwyant <bzwyant@gmail.com>
2021-06-09 17:35:03 -0400

    Added SharpenImage

---
+++ src/HW05/Model/SharpenImage.java
@@ -0,0 +1,27 @@
+package HW05.Model;
+
+import java.awt.Point;
+
+public class SharpenImage extends RGBFilter {
+
+  public static SharpenImage createFilter() {
+    Double[][] filter = new Double[][]{
+        {-.125, -.125, -.125, -.125, -.125},
+        {-.125, .25, .25, .25, -.125},
+        {-.125, .25, 1.0, .25, -.125},
+        {-.125, .25, .25, .25, -.125},
+        {-.125, -.125, -.125, -.125, -.125}
+    };
+    return new SharpenImage(filter, new Point(1, 1));
+  }
+
+  /**
+   * Constructor for filter class.
+   *
+   * @param filter the filter to be applied
+   * @param kernel the position representing the indices of the kernel in the given filter
+   */
+  private SharpenImage(Double[][] filter, Point kernel) {
+    super(filter, kernel);
+  }
+}
