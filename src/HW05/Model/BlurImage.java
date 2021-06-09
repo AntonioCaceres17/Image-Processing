6e30432e748658ea3afbc2e4ab2587dcf8f14618
bzwyant <bzwyant@gmail.com>
2021-06-09 17:35:56 -0400

    Changed BlurFilter to BlurImage

--- src/HW05/Model/BlurImage.java
+++ src/HW05/Model/BlurImage.java
@@ -2,15 +2,15 @@ package HW05.Model;
 
 import java.awt.Point;
 
-public class BlurFilter extends RGBFilter {
+public class BlurImage extends RGBFilter {
 
-  public static BlurFilter createFilter() {
+  public static BlurImage createFilter() {
     Double[][] filter = new Double[][]{
         {.0625, .125, .0625},
         {.125, .25, .125},
         {.0625, .125, .0625}
     };
-    return new BlurFilter(filter, new Point(1, 1));
+    return new BlurImage(filter, new Point(1, 1));
   }
 
   /**
@@ -19,7 +19,7 @@ public class BlurFilter extends RGBFilter {
    * @param filter the filter to be applied
    * @param kernel the position representing the indices of the kernel in the given filter
    */
-  private BlurFilter(Double[][] filter, Point kernel) {
+  private BlurImage(Double[][] filter, Point kernel) {
     super(filter, kernel);
   }
 }
